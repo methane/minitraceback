01c5ef2bb48fc38d85259c7acbe9d0f21966b327
Inada Naoki <songofacandy@gmail.com>
2024-04-11 17:07:30 +0900

    initial import

---
+++ src/minitraceback/__init__.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import traceback
+import pathlib
+import os
+import sys
+
+
+def extract_tb(tb, *, limit=None) -> list[tuple[str, int, str]]:
+    paths = [(p or os.getcwd()) for p in sys.path]
+
+    tbs = [*traceback.walk_tb(tb)]
+    tbs.reverse()
+    if limit is not None:
+        del tbs[limit:]
+
+    result = []
+    for f, lineno in tbs:
+        c = f.f_code
+        filename = pathlib.Path(c.co_filename)
+        for p in paths:
+            try:
+                filename = filename.relative_to(p, walk_up=False)
+                break
+            except ValueError:
+                continue
+        result.append((str(filename), lineno, c.co_name))
+
+    return result
+
+
+def format_miti_tb(tbs: list[tuple[str, int, str]], *, header=True, indent=2) -> list[str]:
+    sindent = " " * indent
+    if header:
+        res = ["Traceback (most recent call first):"]
+    else:
+        res = []
+    for f, lineno, name in tbs:
+        res.append(f"{sindent}{f}:{lineno} {name}")
+    return res
+
+
+def format_exception(exc: BaseException, *, indent=2) -> list[str]:
+    exc_type = type(exc)
+    exc_type_qualname = exc_type.__qualname__
+    exc_type_module = exc_type.__module__
+    if exc_type_module == "builtins":
+        stype = exc_type_qualname
+    else:
+        stype = f"{exc_type_module}.{exc_type_qualname}"
+    res = [f"{stype}: {exc}"]
+
+    sindent = " " * indent
+    for n in getattr(exc, "__notes__", ()):
+        res.append(f"{sindent}{n}")
+
+    return res
---
+++ tests/test_minitraceback.py
@@ -0,0 +1,33 @@
+import minitraceback
+
+import sub
+
+
+def test_extract_tb():
+    e = sub.f1()
+    tbs = minitraceback.extract_tb(e.__traceback__)
+
+    assert tbs[0] == ("sub/__init__.py", 19, "f3")
+    assert tbs[1] == ("sub/__init__.py", 15, "f2")
+    assert tbs[2] == ("sub/__init__.py", 7, "f1")
+    assert len(tbs) == 3
+
+
+def test_format_miti_tb():
+    e = sub.f1()
+    tbs = minitraceback.extract_tb(e.__traceback__)
+    lines = minitraceback.format_miti_tb(tbs)
+
+    assert lines[0] == "Traceback (most recent call first):"
+    assert lines[1] == "  sub/__init__.py:19 f3"
+    assert lines[2] == "  sub/__init__.py:15 f2"
+    assert lines[3] == "  sub/__init__.py:7 f1"
+    assert len(lines) == 4
+
+
+def test_format_exception():
+    e = sub.f1()
+    s = minitraceback.format_exception(e)
+    assert s[0] == "sub.HogeError: foobar"
+    assert s[1] == "  this is note"
+    assert len(s) == 2
