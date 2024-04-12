1724a74106da49010c797e8b0f0a742cfd6c49fe
Inada Naoki <songofacandy@gmail.com>
2024-04-12 21:22:31 +0900

    update

--- src/minitraceback/__init__.py
+++ src/minitraceback/__init__.py
@@ -4,43 +4,69 @@ import os
 import pathlib
 import sys
 import traceback
+import itertools
+from typing import NamedTuple
 
 
-def extract_tb(tb, *, limit=None) -> list[tuple[str, int, str]]:
-    paths = [(p or os.getcwd()) for p in sys.path]
+TRACEBACK_HEADER = "Traceback (most recent call first):"
 
-    tbs = [*traceback.walk_tb(tb)]
-    tbs.reverse()
-    if limit is not None:
-        del tbs[limit:]
 
-    result = []
-    for f, lineno in tbs:
+class FrameInfo(NamedTuple):
+    filename: str
+    lineno: int
+    funcname: str
+
+
+def _extract_from(gen) -> list[FrameInfo]:
+    paths = [os.path.abspath(p) for p in sys.path]
+    paths.sort(key=len, reverse=True)
+
+    result: list[FrameInfo] = []
+    for f, lineno in gen:
         c = f.f_code
-        filename = pathlib.Path(c.co_filename)
-        for p in paths:
-            try:
-                filename = filename.relative_to(p, walk_up=False)
-                break
-            except ValueError:
-                continue
-        result.append((str(filename), lineno, c.co_name))
+        filename: str = c.co_filename
+        if not filename:
+            filename = "?"
+        elif not filename.startswith("<"):
+            fp = pathlib.Path(filename)
+            for p in paths:
+                try:
+                    filename = str(fp.relative_to(p, walk_up=False))
+                    break
+                except ValueError:
+                    continue
+        result.append(FrameInfo(filename, lineno, c.co_name))
 
     return result
 
 
-def format_miti_tb(tbs: list[tuple[str, int, str]], *, header=True, indent=2) -> list[str]:
-    sindent = " " * indent
-    if header:
-        res = ["Traceback (most recent call first):"]
+def extract_tb(tb, /, *, limit: int | None = None) -> list[FrameInfo]:
+    """Extract a traceback from a traceback object."""
+    tbs = list(traceback.walk_tb(tb))
+    if limit is not None:
+        tbs = tbs[: -limit - 1 : -1]
     else:
-        res = []
-    for f, lineno, name in tbs:
-        res.append(f"{sindent}{f}:{lineno} {name}")
-    return res
+        tbs.reverse()
+    return _extract_from(tbs)
 
 
-def format_exception(exc: BaseException, *, indent=2) -> list[str]:
+def extract_stack(f, /, *, limit: int | None = None) -> list[FrameInfo]:
+    """Extract a traceback from a frame object."""
+    frames = itertools.islice(traceback.walk_stack(f), limit)
+    return _extract_from(frames)
+
+
+def format_frames(frames: list[FrameInfo], *, indent=0) -> list[str]:
+    """Format a list of FrameInfo into a list of strings.
+
+    Format is 'filename:lineno funcname'.
+    """
+    sindent = " " * indent
+    return [f"{sindent}{f}:{lineno} {name}" for f, lineno, name in frames]
+
+
+def format_exception_only(exc: BaseException) -> list[str]:
+    """Format an exception without a traceback."""
     exc_type = type(exc)
     exc_type_qualname = exc_type.__qualname__
     exc_type_module = exc_type.__module__
@@ -50,8 +76,18 @@ def format_exception(exc: BaseException, *, indent=2) -> list[str]:
         stype = f"{exc_type_module}.{exc_type_qualname}"
     res = [f"{stype}: {exc}"]
 
-    sindent = " " * indent
     for n in getattr(exc, "__notes__", ()):
-        res.append(f"{sindent}{n}")
+        res.append(f"  {n}")
 
     return res
+
+
+def format_exception(exc, /, *, limit=None) -> list[str]:
+    """Format an exception and its traceback."""
+    tb = exc.__traceback__
+    tbs = extract_tb(tb, limit=limit)
+    return [
+        *format_exception_only(exc),
+        TRACEBACK_HEADER,
+        *format_frames(tbs, indent=2),
+    ]
